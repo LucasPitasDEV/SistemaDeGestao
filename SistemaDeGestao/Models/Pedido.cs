890e9cec8498ea284a56dfd8e2adb31769266300
Lucas Pitas <Lucas Pitas@DESKTOP-9RH53ML>
2025-08-25 18:43:44 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeGestao/Models/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeGestao.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+
+        public Pessoa Pessoa { get; set; }
+
+        public List<ItemPedido> Itens { get; set; }
+
+        public decimal ValorTotal { get; set; }
+
+        public DateTime DataVenda { get; set; }
+
+        public string FormaPagamento { get; set; }
+
+        public string Status { get; set; }
+    }
+}
